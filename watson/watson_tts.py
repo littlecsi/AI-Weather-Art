5d436ca91a1b37387755280c6c283dec1a749ebf
gfh520 <108330090+gfh520@users.noreply.github.com>
2023-06-05 14:45:31 +0100

    Add files via upload

---
+++ watson/watson_tts.py
@@ -0,0 +1,16 @@
+
+from ibm_watson import TextToSpeechV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+auth = IAMAuthenticator(tts_key)
+text_to_speech = TextToSpeechV1(authenticator=auth)
+text_to_speech.set_service_url(tts_url)
+text_to_speech.disable_ssl_verification
+
+
+with open((dirname(__file__), '../sample', 'audio-file2.mp3'), 'wb') as audio_file: 
+    text_results = text_to_speech.synthesise(audio = audio_file, accept ="audio/mp3", voice = "en_US_AllisonVoice").get_result()
+    audio_file.write(text_results.content)
+
+with open((dirname(__file__), '../sample', 'audio-file2.mp3'), 'r') as f:
+     text = f.readlines()
