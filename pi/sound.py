1fa09a4759a0479ebce7db168afd51770189f397
lc2120 <littlecsi27@gmail.com>
2023-06-26 20:22:57 +0100

    [fix] Fixed recording time and file path

--- pi/sound.py
+++ pi/sound.py
@@ -46,8 +46,8 @@ def record():
     sample_format = pyaudio.paInt16 # 16 bits per sample
     channels = 1
     fs = 44100  # Record at 44100 samples per second
-    seconds = 5
-    filename = 'recording.wav'
+    seconds = 4
+    filename = join(dirname(__file__), '../sample', 'recording.wav')
 
     p = pyaudio.PyAudio()   # Create an interface to PortAudio
 
